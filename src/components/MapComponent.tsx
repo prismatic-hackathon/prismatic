1ad5c040d433f3a5ad96718a61dba7e49f634603
Jonathan Dawson <dawsonjed@gmail.com>
2022-07-05 11:24:59 -0500

    add center component

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,5 +1,11 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+
+function CenterComponent() {
+  const leafletMap = useMap();
+  console.log('map center:', leafletMap.getCenter());
+  return null;
+}
 
 function MapComponent() {
   return (
@@ -9,6 +15,7 @@ function MapComponent() {
       scrollWheelZoom={true}
       style={{ height: '100vh' }}
     >
+      <CenterComponent />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
