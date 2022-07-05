0788418dc337e8db0026347a83c154da0047e2f1
Jonathan Dawson <dawsonjed@gmail.com>
2022-07-05 10:06:06 -0500

    add in leaflet css

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,15 +1,22 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function MapComponent() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: '100vh' }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={[51.505, -0.09]}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          A pretty CSS3 popup.
+          <br /> Easily customizable.
         </Popup>
       </Marker>
     </MapContainer>
